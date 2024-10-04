b13ba055ee64974887eab81bbfa1e681ef3f49fd
Fausto Ruvalcaba <2322300+seFausto@users.noreply.github.com>
2024-10-03 22:40:51 -0700

    Create DevToolsInstallScript.ps1

---
+++ DevToolsInstallScript.ps1
@@ -0,0 +1,28 @@
+winget install Microsoft.VisualStudioCode --Source winget
+winget install Microsoft.VisualStudio.2022.Community --Source winget
+winget install 9NBLGGH3ZBJQ --Source msstore #ditto
+winget install Mozilla.Firefox.DeveloperEdition --Source winget
+winget install Microsoft.PowerToys --Source winget
+winget install SamHocevar.WinCompose --Source winget
+winget install OpenWhisperSystems.Signal --Source winget
+winget install Microsoft.AzureDataStudio --Source winget
+winget install Microsoft.WindowsTerminal --Source winget
+winget install OpenJS.NodeJS --Source winget
+winget install Git.Git --Source winget
+winget install dotPDNLLC.paintdotnet --Source winget
+winget install Spotify.Spotify --Source winget
+winget install startallback
+winget install oh-my-posh --Source winget
+winget install Telerik.Fiddler.Classic --Source winget
+winget install Neovim.Neovim --Source winget
+winget install Chocolatey.Chocolatey --winget
+winget install -e --id Obsidian.Obsidian
+winget install -e --id Insomnia.Insomnia
+winget install -e --id Docker.DockerDesktop
+
+## EarTrumpet
+## Mp3Tag
+
+
+choco install synergy -y
+choco install poshgit -y
